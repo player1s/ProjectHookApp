8c75741ddb01796091dfa04369adf726289c4126
Levente <267589@via.dk>
2018-12-03 21:18:00 +0100

    T2 web services+ T1 connection to it (T1 not complete)
    see pic on slack that i uploaded in general at 03 12 2018 pls

---
+++ out/production/ProjectHookApp/T1/ConsumeWebService/Client.cs
@@ -0,0 +1,22 @@
+using System.Threading.Tasks;
+using System.Net.Http;
+class Client
+{
+    static void Main(string[] args)
+    {
+        string s = GetData().GetAwaiter().GetResult();
+        System.Console.WriteLine(s);
+    }
+    static async Task<string> GetData() 
+    {
+
+
+
+        HttpClient client = new HttpClient();
+
+        string str = await client.GetStringAsync("http://localhost:5000/api/values/4");
+
+        return str;
+        
+    }
+}
