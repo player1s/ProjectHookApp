6091e133d54a0aa90879e3d05ef66efcf9f0d428
Levente <267589@via.dk>
2018-12-18 00:45:58 +0100

    sneaky errors caught

--- src/T1/T1GUI/T1GUI/Login.Designer.cs
+++ src/T1/T1GUI/T1GUI/Login.Designer.cs
@@ -46,7 +46,6 @@
             this.label1.Size = new System.Drawing.Size(75, 17);
             this.label1.TabIndex = 0;
             this.label1.Text = "user name";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
@@ -104,7 +103,6 @@
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(0, 17);
             this.label3.TabIndex = 6;
-            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // Login
             // 
